3701e1de961a83f2fbe9773a070e8990a1005a93
Sugumaran <suguanbu321@gmail.com>
2019-01-26 14:36:23 +0530

    mp3 send through pipe rcv same pipe storein newmp3

--- pipe/mp3_rd_wrt/sender.h
+++ pipe/mp3_rd_wrt/sender.h
@@ -1,3 +1,10 @@
+/*************/
+/* Author      :     A.Sugumaran                     */
+/* Date        :     26 jan 2019                          */
+/* Filename    :     mp3 send through pipe recieve through the same pipe, store in new mp3 file  */
+/* Description :     all header files              */
+/*************/
+
 #include <stdio.h>
 #include <string.h>
 #include <sys/types.h>
